9b1bcdbbcadc607b570b126992aa530c6d79a690
~nastya <113263227+boevAV@users.noreply.github.com>
2024-04-16 00:09:23 +0300

    Create variables.tf

---
+++ tf/variables.tf
@@ -0,0 +1,19 @@
+variable "image_name" {
+  default = "ubuntu-22.04"
+}
+
+variable "flavor_name" {
+  default = "m1.small"
+}
+
+variable "key_pair" {
+  default = "boeva-keypair"
+}
+
+variable "security_group" {
+  default = "default"
+}
+
+variable "network_name" {
+  default = "sutdents-net"
+}
